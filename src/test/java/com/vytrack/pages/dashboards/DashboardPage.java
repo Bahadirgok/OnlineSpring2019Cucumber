9fcb7451b0cd94d7a9972718c4cd82065848b390
Bahadirgok <bahadirgok@hotmail.com>
2019-08-25 19:10:42 -0500

    Initial commit

---
+++ src/test/java/com/vytrack/pages/dashboards/DashboardPage.java
@@ -0,0 +1,8 @@
+package com.vytrack.pages.dashboards;
+
+import com.vytrack.utilities.BasePage;
+
+public class DashboardPage extends BasePage {
+
+
+}
---
+++ src/test/java/com/vytrack/runners/FailedRunner.java
@@ -0,0 +1,6 @@
+package com.vytrack.runners;
+
+public class FailedRunner {
+
+
+}
